5f245fea050a099ac06dfd88a533cfcc21ca26df
Huỳnh Hà <hahpse04181@fpt.edu.vn>
2017-01-04 23:05:53 +0700

    Login Demo

---
+++ DemoLoginFacbook/app/src/main/java/com/example/hasan/demologinfacbook/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.hasan.demologinfacbook;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ImageView img;
+    private TextView tv;
+
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+                matrix, false);
+
+        return resizedBitmap;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        try {
+//            PackageInfo packageInfo = getPackageManager().getPackageInfo("com.example.hasan.demologinfacbook", PackageManager.GET_SIGNATURES);
+//            for (Signature signature : packageInfo.signatures) {
+//                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+//                messageDigest.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT));
+//            }
+//        } catch (Exception e) {
+//        }
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        img = (ImageView) findViewById(R.id.imageView);
+        tv = (TextView) findViewById(R.id.textView);
+        loginButton.setReadPermissions("email", "public_profile");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+                    @Override
+                    protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+                    }
+                };
+                accessTokenTracker.startTracking();
+                ProfileTracker profileTracker = new ProfileTracker() {
+                    @Override
+                    protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+
+                    }
+                };
+
+                profileTracker.startTracking();
+                Profile profile = Profile.getCurrentProfile();
+                if(profile!=null){
+                tv.setText(profile.getFirstName() + " " + profile.getLastName());
+                String url = profile.getProfilePictureUri(200, 200).toString();
+                DowloadImage dowloadImage = new DowloadImage();
+                dowloadImage.execute(img, url);}
+                Log.e("facebook name:", profile.getProfilePictureUri(500, 500).toString());
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Login attempt canceled.", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(MainActivity.this, "Login attempt failed.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class DowloadImage extends AsyncTask<Object, Void, Bitmap> {
+        ImageView bmImage;
+
+        @Override
+        protected Bitmap doInBackground(Object... object) {
+            String sURL = (String) object[1];
+            bmImage = (ImageView) object[0];
+            try {
+                InputStream in = (InputStream) new URL(sURL).getContent();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                in.close();
+                return bitmap;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
+
